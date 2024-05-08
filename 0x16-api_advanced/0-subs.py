03a098c58555d7d9076be38dbea4887686a6004c
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-05-08 02:37:05 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,15 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
+    and returns the number of subscribers for a given subreddit.
+
+    Parameters:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+        If the subreddit is not found or there is an error,
+        returns 0.
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
