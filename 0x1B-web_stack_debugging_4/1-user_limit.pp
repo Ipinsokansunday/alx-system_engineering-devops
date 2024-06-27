fa29dd3803ac152b33ea4947192738dce217a9c0
Ipinsokansunday <ipinboluwaji@yahoo.com>
2024-06-27 02:00:58 -0700

    1. User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,18 @@
-# Reconfigure the OS for 'holberton' to login and open a file without any error message
-
-exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+# Ensure puppetlabs-stdlib is available
+if !('puppetlabs-stdlib' in $::modules) {
+  package { 'puppetlabs-stdlib':
+    ensure => present,
+  }
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+file_line { 'increase-hard-file-limit-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton hard nofile 50000',
+  match => '^holberton hard nofile',
+}
+
+file_line { 'increase-soft-file-limit-for-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton soft nofile 50000',
+  match => '^holberton soft nofile',
 }
